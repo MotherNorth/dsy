c4322716189e53f3098f82942ba78b5f67aea88b
LC1301301 <LC1301301>
2018-08-16 09:51:49 -0400

    Primera

---
+++ Ejemplo_Hola_Mundo/src/Main.java
@@ -0,0 +1,11 @@
+
+public class Main {
+
+
+    public static void main(String[] args) {
+       
+        System.out.println("Hola Mundo");
+        System.out.println("Ejemplo para GitHub / GitLab");
+    }
+    
+}
